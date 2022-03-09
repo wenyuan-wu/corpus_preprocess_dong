21414abacee60aa92a7f7cb6b7c6acff39f95605
Wenyuan Wu <wenyuan.wu@uzh.ch>
2022-03-09 01:28:26 +0100

    get_pmc_date implemented

--- json_to_txt.py
+++ json_to_txt.py
@@ -1,5 +1,13 @@
 import pandas as pd
 from tqdm import tqdm
+import os
+
+
+def get_pmc_date(file_name):
+    folder_name = "meta_data"
+    file_path = os.path.join(folder_name, file_name)
+    df = pd.read_csv(file_path, sep=";")
+    return df
 
 
 def unicode_converter():
@@ -7,7 +15,10 @@ def unicode_converter():
 
 
 def main():
-    raise NotImplementedError
+    pmc_date_df = get_pmc_date("pmc_date.csv")
+    print(pmc_date_df)
+    print(pmc_date_df.columns)
+    print(pmc_date_df["publish_time"])
 
 
 if __name__ == '__main__':
