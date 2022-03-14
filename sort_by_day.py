b5fc1ddc8bf10d8657231b022f55f7c674972621
Wenyuan Wu <wenyuan.wu@uzh.ch>
2022-03-15 00:00:15 +0100

    sort_by_day.py updated

--- sort_by_day.py
+++ sort_by_day.py
@@ -1,26 +1,38 @@
 import os
 from tqdm import tqdm
 import datetime
+import pandas as pd
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    # datefmt='%d-%b-%y %H:%M:%S'
+                    )
 
 
-def sort_by_day(file_folder, days):
+def get_date_df(file_folder, days):
     process_bar = tqdm(os.listdir(file_folder))
-    counter = 0
+    date_list = []
+    name_list = []
     for file_name in process_bar:
         process_bar.set_description("Processing {}".format(file_name))
-        # print(file_name)
-        paper_date = datetime.datetime.strptime(file_name[:10], '%Y-%m-%d')
-        print(paper_date)
-        margin = datetime.timedelta(days=days)
-        print(margin)
-        new_date = paper_date + margin
-        print(new_date)
+        paper_date = file_name[:10]
+        paper_name = file_name
+        date_list.append(paper_date)
+        name_list.append(paper_name)
+    dates_list = [date_list, name_list]
+    df = pd.DataFrame(dates_list)
+    df = df.transpose()
+    df.columns = ['Date', 'File_name']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.sort_values(by=['Date'], inplace=True)
+    print(df)
 
 
 def main():
     file_folder = "output_data"
     days = 10
-    sort_by_day(file_folder, days)
+    get_date_df(file_folder, days)
 
 
 if __name__ == '__main__':
--- test_1.py
+++ test_1.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import locale
 import pandas as pd
+import numpy as np
 
 # print(locale.getlocale())
 # print(datetime.strptime('25/May/2015', "%d/%b/%Y"))
@@ -11,9 +12,10 @@ days = [
         '28/Feb/2015', '18/Feb/2015', '08/May/2015', '20/Jan/2015', '24/Jan/2015', '31/Mar/2015', '30/Apr/2015',
         '17/Feb/2015',
         '19/Mar/2015', '05/May/2015', '22/Jan/2015', '14/Aug/2015', '26/Feb/2015', '14/Mar/2015', '28/May/2015',
-     '19/Mar/2015', '19/Mar/2015', '19/Mar/2015', '01/Jan/2015'],
+     '19/Mar/2015', '19/Mar/2015', '19/Mar/2015', '22/Mar/2016', '23/Mar/2016', '02/Feb/2016',
+     '01/Feb/2016'],
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-     'x', 'y', 'z', 'a1']
+     'x', 'y', 'z', 'a1', 'a2', 'a3', 'a4']
         ]
 
 df = pd.DataFrame(days)
@@ -21,8 +23,34 @@ df = df.transpose()
 df.columns = ['Dates', 'fil_names']
 
 df['Dates'] = pd.to_datetime(df['Dates'])
-df.sort_values(by=['Dates'], inplace=True)
+df.sort_values(by=['Dates'], inplace=True, ignore_index=True)
 print(df)
+
+
+first_date = df.iloc[0]['Dates']
+print(first_date)
+print(type(first_date))
+first_year = first_date.year
+print(first_year)
+print(type(first_year))
+
+new_year = pd.Timestamp(f'{first_year}-01-01T00')
+print(new_year)
+if first_date == new_year:
+    print('eq')
+else:
+    print('no')
+    df.loc[-1] = [new_year, 'sample']
+    df.index = df.index + 1
+    df = df.sort_index()
+
+print(df)
+# first_date = df.head(1)['Dates']
+# print(first_date)
+# year = first_date.year
+# print(year)
+
 
 t = df.groupby(pd.Grouper(key="Dates", axis=0, freq="10D", sort=True))['fil_names'].apply(list).reset_index(name='new')
 print(t)
+
